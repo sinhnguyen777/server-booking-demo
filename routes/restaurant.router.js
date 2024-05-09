a92eec1b32480a09e27d893ac97758e3a99b1fc0
Sinh Nguyen <sinhnguyen.dev@gmail.com>
2024-05-09 17:28:36 +0700

    fix: error endpoint API router

--- routes/restaurant.router.js
+++ routes/restaurant.router.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const restaurantController = require('../controllers/restaurant.controller');
 
 router.get('/', restaurantController.getAllRestaurants);
-router.post('create/', restaurantController.createRestaurant);
-router.get('restaurant/:id', restaurantController.getRestaurantById);
-router.patch('update/:id', restaurantController.updateRestaurant);
-router.delete('delete/:id', restaurantController.deleteRestaurant);
+router.post('/create', restaurantController.createRestaurant);
+router.get('/restaurant/:id', restaurantController.getRestaurantById);
+router.patch('/update/:id', restaurantController.updateRestaurant);
+router.delete('/delete/:id', restaurantController.deleteRestaurant);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config();
-
-
 // connect db
 const connectDB = require('./config/db');
 connectDB();
 
 // router
-// const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const restaurantRouter = require('./routes/restaurant.router');
 
 const app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/restaurants', restaurantRouter);
 
 // catch 404 and forward to error handler
